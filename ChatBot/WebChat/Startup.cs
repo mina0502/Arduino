a6b1dfc8851268a22c0cd901d6b757bbe42f53a3
mina0502 <54879715+mina0502@users.noreply.github.com>
2020-01-02 17:42:07 +0900

    Add files via upload

---
+++ ChatBot/WebChat/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(WebChatSystem.Startup))]
+
+namespace WebChatSystem
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HubConfiguration hubConfiguration = new HubConfiguration()
+            {
+                EnableJSONP = true
+            };
+            app.MapSignalR(hubConfiguration);
+        }
+    }//real_time hubConfiguration
+}
